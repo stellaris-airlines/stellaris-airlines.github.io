dc168776ac20563cca241759cb74c061b39a0209
Stellaris airlines <stellaris.web.dev@gmail.com>
2026-08-15 22:21:42 +0900

    Add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBtmBz0ZelsxEnHZe7rsSctXiktrKvHppk',
+  authDomain: 'stellaris-airlines-web.firebaseapp.com',
+  projectId: 'stellaris-airlines-web',
+  storageBucket: 'stellaris-airlines-web.firebasestorage.app',
+  messagingSenderId: '809373770216',
+  appId: '1:809373770216:web:cd43847b82718e3b67c45d',
+  measurementId: 'G-057X7H7YTR'
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth, firebaseConfig };

22474a098e49111cc5aa06efc28b344c3ccaaf5a
Stellaris airlines <stellaris.web.dev@gmail.com>
2026-08-16 00:54:05 +0900

    Set up Firebase and secure user profiles

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBtmBz0ZelsxEnHZe7rsSctXiktrKvHppk',
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth, firebaseConfig };
+export { app, auth, db, firebaseConfig };

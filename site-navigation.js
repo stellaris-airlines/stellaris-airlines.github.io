8d6658b3d062ded9ac1352a23244b24615b06141
Stellaris airlines <stellaris.web.dev@gmail.com>
2026-08-20 11:37:11 +0900

    Bust site refresh assets after homepage layout update

--- site-navigation.js
+++ site-navigation.js
@@ -1,1 +1,9 @@
-import './site-v3.js?v=20260820-site-v3-1';
+import './site-v3.js?v=20260820-site-v3-2';
+
+if(!document.querySelector('link[data-site-v3-v2]')){
+  const link=document.createElement('link');
+  link.rel='stylesheet';
+  link.href=new URL('./site-v3.css?v=20260820-v2',import.meta.url).href;
+  link.dataset.siteV3V2='true';
+  document.head.appendChild(link);
+}

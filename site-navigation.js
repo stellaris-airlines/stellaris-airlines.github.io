5646316daf89a2ec2aa17fb8a0b29b784f5ec853
Stellaris airlines <stellaris.web.dev@gmail.com>
2026-08-20 12:06:57 +0900

    Suppress legacy popup while loading managed popup

--- site-navigation.js
+++ site-navigation.js
@@ -1,6 +1,7 @@
 import './site-v3.js?v=20260820-site-v3-2';
 import './hero-reset.js?v=20260820-hero-reset-v2';
 import './gmail-inquiry-fix.js?v=20260820-gmail-v1';
+import './legacy-popup-kill.js?v=20260820-legacy-popup-v1';
 import './home-popup.js?v=20260820-home-popup-v1';
 
 if(!document.querySelector('link[data-site-v3-v2]')){

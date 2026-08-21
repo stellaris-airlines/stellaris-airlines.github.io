f9a90564ae6bc1689904808c56b3e7fa756b54dd
Stellaris airlines <stellaris.web.dev@gmail.com>
2026-08-21 12:57:05 +0900

    Refresh audited site and popup runtimes

--- site-navigation.js
+++ site-navigation.js
@@ -1,5 +1,5 @@
-import './site-v3.js?v=20260821-site-v3-4';
+import './site-v3.js?v=20260821-site-v3-5';
 import './hero-reset.js?v=20260820-hero-reset-v2';
 import './gmail-inquiry-fix.js?v=20260820-gmail-v1';
 import './legacy-popup-kill.js?v=20260820-legacy-popup-v1';
-import './home-popup.js?v=20260821-home-popup-v3';
+import './home-popup.js?v=20260821-home-popup-v4';
